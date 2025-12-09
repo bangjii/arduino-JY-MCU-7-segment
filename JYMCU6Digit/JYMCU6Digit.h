188b43d22b35f23d5c1c1de3fc66a20175208846
Hudzaifah Al Jihad <al.ghifari22@gmail.com>
2025-12-09 20:45:06 +0700

    Rename class and update header file for JYMCU6Digit

--- JYMCU6Digit/JYMCU6Digit.h
+++ JYMCU6Digit/JYMCU6Digit.h
@@ -1,47 +1,35 @@
-#ifndef JYMCU7SEGMENT_H
-#define JYMCU7SEGMENT_H
+#ifndef JYMCU6Digit_h
+#define JYMCU6Digit_h
 
 #include <Arduino.h>
 
-class JYMCU7SEGMENT {
+class JYMCU6Digit {
 public:
-    JYMCU7SEGMENT(uint8_t sdi, uint8_t clk, uint8_t le, uint8_t oe);
+    JYMCU6Digit(uint8_t sdi, uint8_t clk, uint8_t le, uint8_t oe);
 
-    void begin(uint16_t digits);
+    void begin(uint8_t digits = 6);
 
     void clear();
-    void render();
+    void render();     // <-- penting
 
-    // Full display printing (right aligned)
-    void printInt(long num);
-    void printFloat(float num, uint8_t decimals = 1);
-    void printString(const char* txt);
+    void printDigit(uint8_t pos, uint8_t val);
+    void setChar(uint8_t pos, char ch, bool dot = false);
+    void setRaw(uint8_t pos, uint8_t raw);
+    void printRaw(uint8_t digit, uint8_t pattern);
 
-    // Per-digit printing (left-to-right indexing)
-    void printIntAt(uint16_t pos, long value);
-    void printStringAt(uint16_t pos, const char* str);
-    void printCharAt(uint16_t pos, const char* str);
-    void printCharAt(uint16_t pos, char c, bool dot = false);
-    void printRawAt(uint16_t pos, uint8_t pattern);
-
-    // Manual render control
-    void enableAutoRender(bool enable);
-    void update();
-
-    // Display control
-    void clearRange(uint16_t start, uint16_t length);
-    void setBrightness(uint8_t level);
+    void printNumber(long num, bool leadingZeros=false);
     void displayOn();
     void displayOff();
 
 private:
     uint8_t pinSDI, pinCLK, pinLE, pinOE;
-    uint16_t numDigits;
+    uint8_t numDigits;
 
-    bool autoRender = true;
-    uint8_t* buffer;
+    uint8_t buffer[6];  // <-- FIX UTAMA
 
     void shiftOutByte(uint8_t data);
+    void latch();
+
 };
 
 #endif
